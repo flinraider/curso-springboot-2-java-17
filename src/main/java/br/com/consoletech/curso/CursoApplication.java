20af0ce2b8ea4870fb27d76e256addda6f16517f
flinraider <leonardo.teixeira.dev@gmail.com>
2021-12-03 17:57:51 -0300

    Project Created

---
+++ src/main/java/br/com/consoletech/curso/CursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.consoletech.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+}
